36ffb65bb3b12e4db136b7e5b2439fc732216c2b
Jan Dvořák <mordae@anilinux.org>
2015-08-06 19:44:10 +0200

    Add recurring-alarm-evt constructor
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- mordae/evt.rkt
+++ mordae/evt.rkt
@@ -3,6 +3,9 @@
 ; Specialized Events
 ;
 
+(require mordae/syntax
+         mordae/async)
+
 (provide
   (all-defined-out))
 
@@ -10,6 +13,20 @@
 (: constant-evt (All (a) (-> a (Evtof a))))
 (define (constant-evt value)
   (wrap-evt always-evt (λ _ value)))
+
+
+(: recurring-alarm-evt (-> Flonum (Evtof Flonum)))
+(define (recurring-alarm-evt interval)
+  (async-task-evt
+    (let* ((start (current-inexact-milliseconds))
+           (next (+ start interval)))
+      (async/loop
+        (sync/enable-break
+          (wrap-evt (alarm-evt next)
+                    (λ (alarm)
+                      (begin0
+                        next
+                        (set! next (+ next interval))))))))))
 
 
 ; vim:set ts=2 sw=2 et:
